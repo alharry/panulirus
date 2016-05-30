c3d97fe7f3cbb580d4c04a7aa58b308b1b713b2c
Alastair Harry <avh@fish.wa.gov.au>
2016-05-30 14:24:46 +0800

    Testing

--- R/data.R
+++ R/data.R
@@ -2,6 +2,6 @@
 #'
 #' Nominal annual catch of yellowfin and bigeye tuna reported by FAO
 #'
-#' @format A data frame with 488 rows and 4 variables:
+#' @format A data frame with 488 rows and 4 variables
 #' @source \url{http://www.fao.org/}
 "tuna"
